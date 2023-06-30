574dd35ee5395653092994db2206cd18825d7e82
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-06-30 15:24:30 +0200

    [FEAT]inizio lavoro file index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>badwords</title>
+</head>
+<body>
+    <form action="server.php" method="POST">
+        <input type="text" name="badword" placeholder="badword">
+        <textarea name="text" placeholder="Write a text"></textarea>
+        <button>Send</button>
+    </form>
+</body>
+</html>
